99e9e20305304923d0fa8512618833e4a4c30c60
heisumesh2006 <heisumesh2006@gmail.com>
2026-07-28 18:00:00 +0530

    setup database layer with sqlalchemy

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI
 from backend.api.v1.routes import router
 from backend.core.logger import logger
 from backend.core.config import settings
+from backend.database.base import Base
+from backend.database.connection import engine
+import backend.models.user
 
 app = FastAPI(
     title=settings.APP_NAME,
@@ -10,7 +13,7 @@ app = FastAPI(
 )
 
 logger.info("Starting Axlero API")
-
+Base.metadata.create_all(bind=engine)
 app.include_router(router)
 
 
---
+++ backend/database/connection.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+
+from backend.core.config import settings
+
+engine = create_engine(
+    settings.DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
